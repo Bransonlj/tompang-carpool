d559d5e94e7ac462d39db7b047cfc92443b32873
Branson Lam <Bransonljt@gmail.com>
2025-08-28 00:53:07 +0800

    add driver event handler

--- profile-service/src/main/java/com/tompang/carpool/profile_service/common/KafkaTopics.java
+++ profile-service/src/main/java/com/tompang/carpool/profile_service/common/KafkaTopics.java
@@ -7,4 +7,10 @@ public class KafkaTopics {
     public static final class User {
         public static final String USER_REGISTERED = "user-registered";
     }
+
+    public static final class Driver {
+        public static final String DRIVER_REGISTRATION_SUCCEEDED = "driver-registration-succeeded";
+        public static final String DRIVER_DEREGISTERED = "driver-deregistered";
+    }
+
 }
--- profile-service/src/main/java/com/tompang/carpool/profile_service/projector/UserProfileProjector.java
+++ profile-service/src/main/java/com/tompang/carpool/profile_service/projector/UserProfileProjector.java
@@ -3,8 +3,10 @@ package com.tompang.carpool.profile_service.projector;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.tompang.carpool.event.driver.DriverRegistrationSucceededEvent;
 import com.tompang.carpool.event.user.UserRegisteredEvent;
 import com.tompang.carpool.profile_service.common.KafkaTopics;
+import com.tompang.carpool.profile_service.exception.ResourceNotFoundException;
 import com.tompang.carpool.profile_service.model.UserProfile;
 import com.tompang.carpool.profile_service.repository.UserProfileRepository;
 
@@ -28,8 +30,12 @@ public class UserProfileProjector {
     this.repository.save(userProfile);
   }
 
-  public void handleDriverRegistered() {
-    // TODO implement
+  @KafkaListener(topics = KafkaTopics.Driver.DRIVER_REGISTRATION_SUCCEEDED, groupId = "profile-service")
+  public void handleDriverRegistered(DriverRegistrationSucceededEvent event) {
+    UserProfile profile = repository.findById(event.getUserId())
+        .orElseThrow(() -> new ResourceNotFoundException("Profile of userId not found: " + event.getUserId()));
+    profile.setDriverId(event.getDriverRegistrationId());
+    this.repository.save(profile);
   }
   
 }
