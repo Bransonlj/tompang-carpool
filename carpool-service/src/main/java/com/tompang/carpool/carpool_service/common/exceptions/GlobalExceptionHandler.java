36eff92874a3eb0416d1df233e8586205ca35d5e
Branson Lam <Bransonljt@gmail.com>
2025-10-10 19:32:13 +0800

    fix errorhandling bug

--- carpool-service/src/main/java/com/tompang/carpool/carpool_service/common/exceptions/GlobalExceptionHandler.java
+++ carpool-service/src/main/java/com/tompang/carpool/carpool_service/common/exceptions/GlobalExceptionHandler.java
@@ -16,7 +16,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({DomainException.class, BadRequestException.class})
     public ResponseEntity<ErrorResponseDto> handleBadRequestException(
-            ResourceNotFoundException ex, HttpServletRequest request) {
+            RuntimeException ex, HttpServletRequest request) {
         ErrorResponseDto response = new ErrorResponseDto(
                 Instant.now().toString(),
                 HttpStatus.BAD_REQUEST.value(),
@@ -39,4 +39,18 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGenericException(
+            Exception ex, HttpServletRequest request) {
+        ErrorResponseDto response = new ErrorResponseDto(
+                Instant.now().toString(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                String.format("%s: %s", ex.getClass().getSimpleName(), ex.getMessage()),
+                request.getRequestURI()
+        );
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
