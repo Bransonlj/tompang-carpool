bde79096eb40cf663202f9bcf68b14e8b07f7e93
Branson Lam <Bransonljt@gmail.com>
2025-10-11 20:10:30 +0800

    update event-translator to not create dto for targetId=senderId

--- websocket-service/src/event/event-translator.service.ts
+++ websocket-service/src/event/event-translator.service.ts
@@ -4,18 +4,25 @@ import { ChatMessageSentEvent, NotificationReceivedEvent } from "src/common/even
 
 @Injectable()
 export class EventTranslatorService {
+
+  /**
+   * Creates a socket event dto for each user in the group, except the sender
+   * @param payload 
+   * @returns 
+   */
   translateChatMessageSent(payload: ChatMessageSentEvent): SendSocketEventDto[] {
-    return payload.groupChatUsers.map<SendSocketEventDto>(groupUserId => ({
-      eventName: SocketEventName.CHAT_MESSAGE,
-      targetUserId: groupUserId,
-      payload: {
-        messageId: payload.messageId,
-        groupChatId: payload.groupChatId,
-        senderUserId: payload.senderUserId,
-        createdAt: new Date(payload.createdAt),
-        message: payload.message,
-      },
-    }));
+    return payload.groupChatUsers.filter(groupUserId => groupUserId !== payload.senderUserId)
+      .map<SendSocketEventDto>(groupUserId => ({
+        eventName: SocketEventName.CHAT_MESSAGE,
+        targetUserId: groupUserId,
+        payload: {
+          messageId: payload.messageId,
+          groupChatId: payload.groupChatId,
+          senderUserId: payload.senderUserId,
+          createdAt: new Date(payload.createdAt),
+          message: payload.message,
+        },
+      }));
   }
 
   translateNotificationReceived(payload: NotificationReceivedEvent): SendSocketEventDto {
