540a3a70faac4155560f837aa4385279021c7ec6
Branson Lam <Bransonljt@gmail.com>
2025-10-12 13:32:05 +0800

    update file-input to expose clearfile ref

--- tompang-web/src/components/file-input.tsx
+++ tompang-web/src/components/file-input.tsx
@@ -1,4 +1,4 @@
-import type { ChangeEvent } from "react";
+import { forwardRef, useImperativeHandle, type ChangeEvent } from "react";
 import { formatFileSize, useSingleFileUpload } from "../hooks/use-file-upload";
 import Alert from "@mui/material/Alert";
 import IconButton from "@mui/material/IconButton";
@@ -6,16 +6,26 @@ import { X } from "lucide-react";
 
 interface FileInputProps {
   fileType?: "image";
+  disabled?: boolean;
   onFileChange?: (file: File | null) => void;
 }
 
-export default function FileInput({
-  fileType="image",
-  onFileChange,
-}: FileInputProps) {
+// Define the methods that the parent can access via ref
+export interface FileInputRef {
+  clearFile: () => void;
+}
+
+const FileInput = forwardRef<FileInputRef, FileInputProps>(
+  ({ fileType = "image", disabled=false, onFileChange }, ref) => {
   const { file, preview, uploadFile, uploadError, clearFile } = useSingleFileUpload({
     fileType,
   });
+
+
+  // Expose clearFile() to parent components
+  useImperativeHandle(ref, () => ({
+    clearFile,
+  }));
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement> | null) => {
     const selectedFile = e?.target?.files?.[0] ?? null;
@@ -30,7 +40,8 @@ export default function FileInput({
   return (
     <div>
       <input 
-        className="bg-white rounded-lg border-2 border-gray-400"
+        disabled={disabled}
+        className="bg-white rounded-lg border-2 border-gray-400 hover:cursor-pointer"
         type="file" 
         onChange={handleFileChange}
       ></input>
@@ -55,4 +66,6 @@ export default function FileInput({
       }
     </div>
   )
-}
+});
+
+export default FileInput;
